849f572e7443c3cd7c1d76bc40bd966f770db019
gibbe75 <41576519+gibbe75@users.noreply.github.com>
2020-04-23 23:29:35 +0200

    adjustement basics

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,25 @@
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
+from django import forms
+from django.db import models
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, ModelFormMixin
+from django.urls import reverse, reverse_lazy
+from .models import *
+# from .forms import *
+# from .services import *
+from datetime import datetime
+from django.contrib import messages
+from django.conf import settings
 
 
-class HomePageView(request):
+def HomePageView(request):
 	messages.success(request, 'Cadeau supprimé.')
 
-    return render(request, 'pages/home.html')
+	return render(request, 'pages/home.html')
 
 
 class AboutPageView(TemplateView):
