666fda5da90c035ec52505c2951e6cd8fd5ef901
gibbe75 <41576519+gibbe75@users.noreply.github.com>
2020-05-03 18:09:45 +0200

    internationalisation

--- config/settings_local.py
+++ config/settings_local.py
@@ -106,9 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # INTERNATIONALIZATION
 # ------------------------------------------------------------------------------
+LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 # https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
 TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-USE_I18N
