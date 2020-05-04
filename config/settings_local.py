0759f455d72995bdffd69bc12f4dae8c07d9b868
gibbe75 <41576519+gibbe75@users.noreply.github.com>
2020-05-04 11:41:01 +0200

    internationalisation

--- config/settings_local.py
+++ config/settings_local.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
